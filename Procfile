bed31ac4aa9d49566fa0cf7457f3d4dfbb080e98
Alex Chick <chick.alexander@gmail.com>
2015-06-26 05:16:26 -0400

    1.3 -- Commit #4
    Includes Heroku app

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python cloud/my_answers/do.py --log-file
+web: python cloud/my_answers/update_in_Parse.py --log-file -
--- cloud/main.js
+++ cloud/main.js
@@ -4,4 +4,20 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
-//Failed to add file P___Python_programs/ to index.
+
+
+
+Parse.Cloud.define("makeTestEventObject", function(request, response) {
+    // Set up to modify user data
+    Parse.Cloud.httpRequest({
+        url: 'https://daeious-ex-machina.herokuapp.com/',
+        success: function(httpResponse) {
+            console.log(httpResponse.text);
+            response.success("Test Event Object created successfully in Parse.");
+        },
+        error: function(httpResponse) {
+            console.error('Request failed with response code ' + httpResponse.response);
+            response.success("Oops.... something went wrong.");
+        }
+    })
+});
