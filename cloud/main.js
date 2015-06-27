f95404bb3e9c7336fd23e898cb93b531292bb59d
Alex Chick <chick.alexander@gmail.com>
2015-06-26 20:27:22 -0400

    1.6.0 -- main.js now uses a non-Parse function inside a Parse Cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,7 @@
 
+Parse.initialize("AKJFNWcTcG6MUeMt1DAsMxjwU62IJPJ8agbwJZDJ", "eyHtd5bpdJufw1JDinRBMNnFx2poEGJETHxAnzuV")
+
+
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
@@ -26,25 +29,60 @@ Parse.Cloud.define("makeTestEventObject", function(request, response) {
 
 
 
-Parse.Cloud.define("copy_Parse_class_into_Firebase", function(request, response) {
-    // Copy all objects in a given classname in Parse to Firebase.
+// Parse.Cloud.define("copy_Parse_class_into_Firebase", function(request, response) {
+//     // Copy all objects in a given classname in Parse to Firebase.
 
-    var query = new Parse.Query("Question");
+//     var query = new Parse.Query("Question");
+//     query.limit(1000);
+//     query.find({
+//         success: function(results) {
+//             response.success("Success -- " + results.length + " Questions were found in Parse");
+//         },
+//         error: function() {
+//             response.error("Question lookup failed");
+//         }
+//     });
+//     console.log("does this print?");
+
+// });
+
+// Parse.Cloud.run("copy_Parse_class_into_Firebase", {}, {});
+
+
+
+function query_all(classname_to_query, callback) {
+    // here you can perform a Parse.Query,
+    // then call callback.success from within
+    // its success block, or callback.error if
+    // it fails
+
+    // ...
+
+    var query = new Parse.Query(classname_to_query);
+    query.limit(1000);
     query.find({
         success: function(results) {
-            response.success(results.length);
+            // callback.success(results.length);
+            var a = "Success: " + results.length + " " + classname_to_query + "s were found in Parse";
+            callback.success(a);
         },
         error: function() {
-            response.error("Question lookup failed");
+            callback.error("Error: " + classname_to_query + " lookup failed");
         }
     });
-});
+}
 
 
-
-
-
-
+Parse.Cloud.define("copy_to_Firebase", function (request, response) {
+    query_all("Answer", {
+        success: function(request) {
+            response.success(request);
+        },
+        error: function(error) {
+            response.error(error);
+        }
+    });
+})
 
 
 
---
+++ public/run.js
@@ -0,0 +1,32 @@
+
+
+
+
+Parse.Cloud.run('copy_Parse_class_into_Firebase', { }, {
+  success: function(num_questions) {
+    // ratings should be 4.5
+  },
+  error: function(error) {
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
