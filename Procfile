3d49764f18e93c70364974f700cf301a9d945bdb
Alex Chick <chick.alexander@gmail.com>
2015-06-27 01:35:06 -0400

    1.7.0 -- Ooooh, it's pretty...can connect to Firebase, run other Parse CloudCode functions, and run a Python program hosted on Heroku

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: python cloud/my_answers/do.py --log-file
-web: python cloud/my_answers/update_in_Parse.py --log-file -
--- cloud/main.js
+++ cloud/main.js
@@ -1,11 +1,19 @@
 
 Parse.initialize("AKJFNWcTcG6MUeMt1DAsMxjwU62IJPJ8agbwJZDJ", "eyHtd5bpdJufw1JDinRBMNnFx2poEGJETHxAnzuV")
 
+// var Firebase = require("firebase.js");
+
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+});
+
+
+Parse.Cloud.define("Logger", function(request, response) {
+  console.log(request.params);
+  response.success();
 });
 
 
@@ -46,45 +54,74 @@ Parse.Cloud.define("makeTestEventObject", function(request, response) {
 
 // });
 
-// Parse.Cloud.run("copy_Parse_class_into_Firebase", {}, {});
 
-
-
-function query_all(classname_to_query, callback) {
+function query_all(classname, callback) {
     // here you can perform a Parse.Query,
     // then call callback.success from within
     // its success block, or callback.error if
     // it fails
-
-    // ...
-
-    var query = new Parse.Query(classname_to_query);
-    query.limit(1000);
+    var cn = classname;
+    var query = new Parse.Query(cn).limit(1000);
     query.find({
         success: function(results) {
-            // callback.success(results.length);
-            var a = "Success: " + results.length + " " + classname_to_query + "s were found in Parse";
-            callback.success(a);
+            callback.success("Success: " + results.length + " " + cn + "s were found in Parse");
         },
         error: function() {
             callback.error("Error: " + classname_to_query + " lookup failed");
         }
     });
-}
+};
 
 
+// Log in, then do several asynchronous tasks.
 Parse.Cloud.define("copy_to_Firebase", function (request, response) {
-    query_all("Answer", {
-        success: function(request) {
-            response.success(request);
-        },
-        error: function(error) {
-            response.error(error);
-        }
+
+    //  (0) Log in.
+    Parse.User.logIn("alex", "1234").then(function(user) {
+    //  (1) Query Parse for "Question" class.
+        var query = new Parse.Query("Question").limit(1000);
+        var promise = query.find();
+        return promise; // This works just as well as > return query.find();
+    }).then(function(results) {
+    //  (2) Query Parse for "Answer" class.
+        var query = new Parse.Query("Answer").limit(1000);
+        return query.find();
+    }).then(function(results) {
+    //  (3) Query Parse for "IPad" class.
+        var query = new Parse.Query("IPad").limit(1000);
+        return query.find();
+    }).then(function(results) {
+    //  (4) Connect to Firebase, just to show it can be done.
+        return Parse.Cloud.httpRequest({
+            url: "https://burning-fire-8681.firebaseio.com/Event.json"
+        });
+    }).then(function(results) {
+    //  (5) Run a simple Python script (hosted on Heroku) that creates an empty "Event" object in Parse.
+        return Parse.Cloud.httpRequest({
+            url: "https://daeious-ex-machina.herokuapp.com/"
+        });
+    }).then(function(results) {
+        return Parse.Cloud.run("hello");
+    }).then(function(results) {
+        return Parse.Cloud.run("makeTestEventObject");
+    }).then(function(results) {
+        response.success("All asynchronous tasks completed successfully!");
+    }, function(error) {
+        response.error(error);
     });
-})
+});
 
-
+    // Parse.Cloud.httpRequest({
+    //     url: 'https://daeious-ex-machina.herokuapp.com/',
+    //     success: function(httpResponse) {
+    //         console.log(httpResponse.text);
+    //         response.success("Test Event Object created successfully in Parse.");
+    //     },
+    //     error: function(httpResponse) {
+    //         console.error('Request failed with response code ' + httpResponse.response);
+    //         response.success("Oops.... something went wrong.");
+    //     }
+    // })
 
 
 
